a8750967aa3091b89d953def19b3e43f093dab01
Lengar022 <garrettlenihan@gmail.com>
2026-03-03 22:44:31 +0000

    Converted temp_scanner.py to a class.

--- telemetry/sensors/temp_scanner.py
+++ telemetry/sensors/temp_scanner.py
@@ -1,40 +1,30 @@
 import os
 import glob
-import time
 
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
- 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+class TempMonitor:
+    def __init__(self):
+        os.system('modprobe w1-gpio')
+        os.system('modprobe w1-therm')
+        
+        base_dir = '/sys/bus/w1/devices/'
+        self.device_folder = glob.glob(base_dir + '28*')[0]
+        self.device_file = self.device_folder + '/w1_slave'
 
-# Read sensor name
-def read_name():
-    name_file=device_folder+'/name'
-    f = open(name_file,'r')
-    return f.readline()
+    def read_temp_raw(self):
+        with open(self.device_file, "r") as f:
+            return f.readlines()
 
-# Read raw sensor data
-def read_raw_data():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-# Parse the data
-def read_temp():
-    lines = read_raw_data()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_raw_data()
-    
-    idx = lines[1].find('t=')
-    
-    if idx != -1:
+    def read_temp(self):
+        lines = self.read_temp_raw()
+        if lines[0].strip()[-3:] != 'YES':
+            return None
+        
+        idx = lines[1].find('t=')
+        
+        if idx == -1:
+            return None
+        
         temp_string = lines[1][idx+2:]
         temp_c = float(temp_string) / 1000.0
         return temp_c
- 
-while True:
-    print(' C=%3.3f'% read_temp())
+    
