d39f032a64e39162b3b0685a816cc11c02aeda05
Garrett Lenihan <garrettleihan@gmail.com>
2026-03-23 20:50:45 +0000

    initial webapp commit

---
+++ webDashboard/web_dash/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+</script>
+
+<div class="container">
+  <div class="box has-text-centered">
+    <div class="title">Car Telemetry Dashboard</div>
+    <div class="subtitle">Live telemetry and GPS location</div>
+  </div>
+</div>
